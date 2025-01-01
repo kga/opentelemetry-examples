5d41e8f5c99b33dcfa81e94feec7ae282a34997e
Wataru TOYA <watrty@gmail.com>
2025-01-01 14:31:27 +0900

    wai

--- perl/lib/Heavy/Web.pm
+++ perl/lib/Heavy/Web.pm
@@ -15,32 +15,31 @@ our $tracer = OTel->init;
 
 # 引数で与えられた秒数だけスリープする関数
 sub superHeavyFunc($tracer, $n) {
-	my $span = $tracer->create_span(
-		name => 'Heavy func ' . $n,
-	);
-	defer { $span->end };
+	$tracer->in_span(
+		"Heavy func $n" => sub ($span, $ctx) {
+			Time::HiRes::sleep($n);
 
-	Time::HiRes::sleep($n);
-
-	if ($n > 5) {
-		my $msg = 'timeout!';
-		$span->record_exception($msg);
-		$span->set_status(OpenTelemetry::Constants::SPAN_STATUS_ERROR, $msg);
-	}
+			if ($n > 5) {
+				my $msg = 'timeout!';
+				$span->record_exception($msg);
+				$span->set_status(OpenTelemetry::Constants::SPAN_STATUS_ERROR, $msg);
+			}
+		},
+	)
 }
 
 get '/heavy' => sub($self, $c) {
-	my $span = $tracer->create_span(
-		name => 'heavy-func',
-		kind => OpenTelemetry::Constants::SPAN_KIND_SERVER,	
+	$tracer->in_span(
+		'heavy-func' => (
+			kind => OpenTelemetry::Constants::SPAN_KIND_SERVER,
+		) => sub ($span, $ctx) {
+			Time::HiRes::sleep(2);
+
+			superHeavyFunc($tracer, 8);
+			superHeavyFunc($tracer, 3);
+			superHeavyFunc($tracer, 5);
+
+			$c->render_json(+{ heavy => 'Hello, World!' });	
+		},
 	);
-	defer { $span->end };
-
-	Time::HiRes::sleep(2);
-
-	superHeavyFunc($tracer, 8);
-	superHeavyFunc($tracer, 3);
-	superHeavyFunc($tracer, 5);
-
-	$c->render_json(+{ heavy => 'Hello, World!' });	
 };
